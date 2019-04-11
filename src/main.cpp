0d3c333196a2b0652cfe697e781d9b90d48b3235
Jarrett Johnson <jarrettsjohnson@gmail.com>
2019-04-11 18:19:59 -0400

    Application class

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,26 @@
-#include <SDL.h>
-#include <SDL_main.h>
-#include <SDL_video.h>
+#include "Application.hpp"
 
 #include <iostream>
 
 int SDL_main(int argc, char* argv[])
 {
-	/* Initialize SDL */
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
-		std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return -1;
-	}
-
-	/* Create window */
-	SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_RESIZABLE);
-	if (window == nullptr)
-	{
-		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		return -1;
-	}
-
-	/* Create renderer */
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (renderer == nullptr)
-	{
-		std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		return -1;
-	}
+	ccm::Application app;
 
 	/* Game loop */
 	SDL_Event event;
-	bool quit = false;
-	while (!quit)
+	while (!app.m_quit)
 	{
 		while (SDL_PollEvent(&event))
 		{
 			switch (event.type)
 			{
 				case SDL_QUIT:
-					quit = true;
+					app.m_quit = true;
 					break;
 			}
 		}
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+		app.render();
 	}
 
-	/* Finalize and exit */
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 	return 0;
 }

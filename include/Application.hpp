1c6dfab2692cdeb81a13068a617fe4564b030dd7
Sahil Iyer <iyersahil12@gmail.com>
2019-04-12 21:15:10 +1000

    Moved event loop to Application class and fixed some brace styling.

--- include/Application.hpp
+++ include/Application.hpp
@@ -4,8 +4,10 @@
 #include <SDL_main.h>
 #include <SDL_video.h>
 
-namespace ccm {
-	class Application {
+namespace ccm
+{
+	class Application
+	{
 	public:
 		Application();
 		Application(const Application&) = delete;
@@ -15,7 +17,10 @@ namespace ccm {
 		~Application();
 
 		void render();
+		void handleEvents();
+
 		bool m_quit{ false };
+		SDL_Event m_event;
 
 	private:
 		SDL_Window* m_window{ nullptr };
--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,9 @@ int SDL_main(int argc, char* argv[])
 	ccm::Application app;
 
 	/* Game loop */
-	SDL_Event event;
 	while (!app.m_quit)
 	{
-		while (SDL_PollEvent(&event))
-		{
-			switch (event.type)
-			{
-				case SDL_QUIT:
-					app.m_quit = true;
-					break;
-			}
-		}
+		app.handleEvents();
 		app.render();
 	}
 

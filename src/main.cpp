31e81d5061d955d0b7126d7e155fc568b36c6991
punter.jack <punter.jack@hotmail.co.uk>
2019-04-13 11:38:22 +0100

    Added Grid Object. Added constructors to cmm::Rect Added example usage of grid into main.cp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Application.h"
 #include "Renderer.h"
 #include "Object.h"
+#include "Grid.h"
 
 #include <iostream>
 
@@ -10,12 +11,31 @@ int SDL_main(int argc, char* argv[])
 	ccm::Renderer renderer{ app };
 	auto[width, height] = app.getSize();
 	ccm::Object obj{ ccm::Rect{width / 2, height / 2, 128, 128}, ccm::Colors::Green };
-
+	
+	// Generate and fill some of the grid elements
+	ccm::Grid grid(ccm::Rect{ 0, 0, width / 2, height }, 10, 20);
+	auto[columns, rows] = grid.getDimensions();
+	for (int x = 0; x < columns; x++)
+	{
+		if (x == 5 || x == 1)
+		{
+			for (int y = 0; y < rows; ++y)
+			{
+				grid(x, y).fill();
+			}
+		}
+	}
+	
 	/* Game loop */
 	while (!app.m_quit)
 	{
 		app.handleEvents();
 		renderer.clear();
+		// Draw the grid
+		for (const auto& o : grid.draw())
+		{
+			renderer.draw(o);
+		}
 		renderer.draw(obj);
 		renderer.render();
 	}

a538fbfa0a80013404a471648fe33c197c6f7f68
Sebastian Kylander <gaztinerag@gmail.com>
2019-04-15 21:37:53 +0200

    Fix minor faults in Singleton.h

--- include/Singleton.h
+++ include/Singleton.h
@@ -1,24 +1,27 @@
 #pragma once
 #include <memory>
 
-template<typename T, typename Deleter = std::default_delete<T>>
-class Singleton
+namespace ccm
 {
-public:
-	Singleton() = default;
-	virtual ~Singleton() = default;
+	template<typename T, typename Deleter = std::default_delete<T>>
+	class Singleton
+	{
+	public:
+		Singleton() = default;
+		virtual ~Singleton() = default;
+	
+		template<typename... Args>
+		static T& initialize(Args&&... args) { return *(s_instance = std::make_unique<T>(std::forward<Args>(args)...)); }
+		static void deinitialize() { s_instance.reset(); }
+	
+		[[nodiscard]] static T& getInstance() { return *s_instance; }
+	
+		[[nodiscard]] static bool isInitialized() { return !!s_instance; }
+	
+	private:
+		static std::unique_ptr<T, Deleter> s_instance;
+	};
 
-	template<typename... Args>
-	static T& initialize(Args&&... args) { return *(s_instance = std::make_unique<T>(std::forward<Args>(args)...)); }
-	static void deinitialize() { s_instance.reset(); }
-
-	static T& getInstance() { return *s_instance; }
-
-	static bool isInitialized() { return !!s_instance; }
-
-private:
-	static std::unique_ptr<T, Deleter> s_instance;
-};
-
-template<typename T, typename Deleter>
-std::unique_ptr<T, Deleter> Singleton<T, Deleter>::s_instance;
+	template<typename T, typename Deleter>
+	std::unique_ptr<T, Deleter> Singleton<T, Deleter>::s_instance;
+}
